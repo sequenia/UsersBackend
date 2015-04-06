888ec8891a26ad070d71d40207dced8e7ff860bf
4ybakut2004 <4ybakut2004@gmail.com>
2015-04-06 12:04:03 +0600

    Users with login and password

--- db/migrate/20150406051609_devise_create_users.rb
+++ db/migrate/20150406051609_devise_create_users.rb
@@ -2,15 +2,18 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :login,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      #t.string   :reset_password_token
+      #t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
+
+      ## Token authenticatable
+      t.string :authentication_token
 
       ## Trackable
       #t.integer  :sign_in_count, default: 0, null: false
@@ -20,10 +23,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       #t.inet     :last_sign_in_ip
 
       ## Confirmable
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
+      #t.string   :confirmation_token
+      #t.datetime :confirmed_at
+      #t.datetime :confirmation_sent_at
+      #t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -34,9 +37,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
+    add_index :users, :login,                unique: true
+    add_index :users, :authentication_token, unique: true
+    #add_index :users, :reset_password_token, unique: true
+    #add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
