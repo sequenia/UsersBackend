4f0963838bf9bee0da608002a0c37f75bdbb0722
4ybakut2004 <4ybakut2004@gmail.com>
2015-04-06 21:26:38 +0600

    Капистрано

--- Gemfile
+++ Gemfile
@@ -49,5 +49,11 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  # Autodeploy
+  gem 'capistrano', '3.1.0'
+  gem 'capistrano-rails'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rvm'
 end
 
---
+++ config/deploy.rb
@@ -0,0 +1,180 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+application = "#{ENV["APPNAME"]}"
+set :application, "#{application}"
+set :deploy_to, "/var/www/apps/#{application}"
+set :deploy_via, :copy
+set :scm, :git
+set :repo_url, 'git@github.com:sequenia/UsersBackend.git'
+set :branch, 'production'
+set :rvm_type, :user
+set :rvm_ruby_version, '2.0.0-p481'
+
+namespace :foreman do
+  desc 'Start server'
+  task :start do
+    on roles(:all) do
+      sudo "start #{application}"
+    end
+  end
+
+  desc 'Stop server'
+  task :stop do
+    on roles(:all) do
+      sudo "stop #{application}"
+    end
+  end
+
+  desc 'Restart server'
+  task :restart do
+    on roles(:all) do
+      sudo "restart #{application}"
+    end
+  end
+
+  desc 'Server status'
+  task :status do
+    on roles(:all) do
+      execute "initctl list | grep #{application}"
+    end
+  end
+end
+
+namespace :git do
+  desc 'Deploy'
+  task :deploy do
+    ask(:message, "Commit message?")
+    run_locally do
+      execute "git add -A"
+      execute "git commit -m '#{fetch(:message)}'"
+      execute "git push origin master"
+    end
+  end
+end
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+  desc 'Setup'
+  task :setup do
+    on roles(:all) do
+      execute "mkdir  #{shared_path}/config/"
+      execute "mkdir  /var/www/apps/#{application}/run/"
+      execute "mkdir  /var/www/apps/#{application}/log/"
+      execute "mkdir  /var/www/apps/#{application}/socket/"
+      execute "mkdir  /var/www/log/"
+      execute "mkdir  /var/www/log/upstart/"
+      execute "mkdir #{shared_path}/system"
+      sudo "ln -s /var/log/upstart /var/www/log/upstart"
+
+      upload!('shared/database.yml', "#{shared_path}/config/database.yml")
+      upload!('shared/Procfile', "#{shared_path}/Procfile")
+      upload!('shared/nginx.conf', "#{shared_path}/nginx.conf")
+      upload!('shared/.env', "#{shared_path}/.env")
+
+      sudo 'stop nginx'
+      sudo "rm -f /usr/local/nginx/conf/nginx.conf"
+      sudo "ln -s #{shared_path}/nginx.conf /usr/local/nginx/conf/nginx.conf"
+      sudo 'start nginx'
+
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:create:all"
+        end
+      end
+    end
+  end
+
+  desc 'Create symlink'
+  task :symlink do
+    on roles(:all) do
+      execute "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+      execute "ln -s #{shared_path}/Procfile #{release_path}/Procfile"
+      execute "ln -s #{shared_path}/system #{release_path}/public/system"
+      execute "ln -s #{shared_path}/.env #{release_path}/.env"
+    end
+  end
+
+  desc 'Foreman init'
+  task :foreman_init do
+    on roles(:all) do
+      foreman_temp = "/var/www/tmp/foreman"
+      execute  "mkdir -p #{foreman_temp}"
+      # Создаем папку current для того, чтобы foreman создавал upstart файлы с правильными путями
+      execute "ln -s #{release_path} #{current_path}"
+
+      within current_path do
+        execute "cd #{current_path}"
+        execute :bundle, "exec foreman export upstart #{foreman_temp} -a #{application} -u ИМЯ_ПОЛЬЗОВАТЕЛЯ_СИСТЕМЫ -l /var/www/apps/#{application}/log -d #{current_path}"
+      end
+      sudo "mv #{foreman_temp}/* /etc/init/"
+      sudo "rm -r #{foreman_temp}"
+    end
+  end
+
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      sudo "restart #{application}"
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+  after :finishing, 'deploy:restart'
+
+  after :updating, 'deploy:symlink'
+
+  after :setup, 'deploy:foreman_init'
+
+  after :foreman_init, 'foreman:start'
+
+  before :foreman_init, 'rvm:hook'
+
+  before :setup, 'deploy:starting'
+  before :setup, 'deploy:updating'
+  before :setup, 'bundler:install'
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
+
+#before :deploy, 'git:deploy'
